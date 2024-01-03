02cf6d4d68535075cbe6017915bcfa15923088d1
rowan <olaf.luo@foxmail.com>
2024-01-03 20:44:08 +0800

    script the xdpdsrlb demo

--- split/netenv/4-adv-netwotk/77-L4-L7-LB/1-L4/5-XDP_LBDSR_Enhance/1-setup-clab.sh
+++ split/netenv/4-adv-netwotk/77-L4-L7-LB/1-L4/5-XDP_LBDSR_Enhance/1-setup-clab.sh
@@ -1,92 +1,32 @@
 #!/bin/bash
 set -v
 
-# topo:    
-#          ---10.1.5.99/24-------------10.1.5.80---web1(nginx)
-#          |                         /
-# client---| keepalived+haproxy 10.1.5.100/24  
-#          |                         \
-#          ---10.1.5.98/24-------------10.1.5.90---web2(nginx)
+docker run -td -h xdpdsrlb --privileged --name xdpdsrlb 192.168.2.100:5000/ebpf-xdp:v03
+docker exec xdpdsrlb bash -c "git clone https://github.com/snpsuen/XDP_LBDSR_Enhance && cd XDP_LBDSR_Enhance && make"
 
-cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
-name: xdplbdsr
-mgmt:
-  ipv6-subnet: ""
-  ipv4-subnet: 172.20.20.0/24
+tail -f  /sys/kernel/debug/tracing/trace_pipe > xdpdsrlb.trace >/dev/null 2>&1 &
 
-topology:
-  nodes:
-    xdplb:
-      kind: linux
-      image: 192.168.2.100:5000/ebpf-xdp:v03
-      exec:
-      - ip a a 10.1.5.99/24 dev net1
-      - keepalived -D  -f /etc/keepalived/keepalived.conf
-      - haproxy -f /etc/haproxy/haproxy.cfg
-      cmd: sleep infinity 
-      binds:
-        - ./keepalived/keepalived1/keepalived.conf:/etc/keepalived/keepalived.conf
-        - ./haproxy/haproxy1/haproxy.cfg:/etc/haproxy/haproxy.cfg
+docker run -td --privileged --name backend_a -h backend-A 192.168.2.100:5000/nettool
+docker run -td --privileged --name backend_b -h backend-B 192.168.2.100:5000/nettool
+docker exec backend_a bash -c "ip addr add 192.168.25.10/24 dev lo"
+docker exec backend_b bash -c "ip addr add 192.168.25.10/24 dev lo"
 
-    keepalived2-haproxy2:
-      kind: linux
-      image: 192.168.2.100:5000/keepalived-2.0.8-haproxy-1.5.18
-      cmd: sleep infinity
-      exec:
-      - ip a a 10.1.5.101/24 dev net1
-      - keepalived -D  -f /etc/keepalived/keepalived.conf
-      - haproxy -f /etc/haproxy/haproxy.cfg
-      binds:
-        - ./keepalived/keepalived2/keepalived.conf:/etc/keepalived/keepalived.conf
-        - ./haproxy/haproxy2/haproxy.cfg:/etc/haproxy/haproxy.cfg
+docker run -td --privileged --name client -h client 192.168.2.100:5000/nettool
+docker exec client bash -c "ip route add 192.168.25.10/32 via 172.17.0.2"
 
-    nginx1:
-      kind: linux
-      image: 192.168.2.100:5000/nginx:1.7.9
-      exec:
-      - ip addr add 10.1.5.80/24 dev net0
-      - ip route replace default via 10.1.5.1
-      binds:
-        - ./nginx/nginx1/default.conf:/etc/nginx/conf.d/default.conf
+# make a test:
+echo backend_a_ip=$(docker exec -it backend_a ip a s eth0 | grep inet | awk -F "/" '{print $1}' | awk -F " " '{print $2}')
+echo backend_a_mac=$(docker exec -it backend_a ip l show eth0 | grep "link/ether" | awk '{print $2}')
+echo backend_b_ip=$(docker exec -it backend_b ip a s eth0 | grep inet | awk -F "/" '{print $1}' | awk -F " " '{print $2}')
+echo backend_b_mac=$(docker exec -it backend_b ip l show eth0 | grep "link/ether" | awk '{print $2}')
 
-    nginx2:
-      kind: linux
-      image: 192.168.2.100:5000/nginx:1.7.9
-      exec:
-      - ip addr add 10.1.5.90/24 dev net0
-      - ip route replace default via 10.1.5.1
-      binds:
-        - ./nginx/nginx2/default.conf:/etc/nginx/conf.d/default.conf
+echo xdpdsrlb_ip=$(docker exec -it xdpdsrlb ip a s eth0 | grep inet | awk -F "/" '{print $1}' | awk -F " " '{print $2}')
+echo xdpdsrlb_mac=$(docker exec -it xdpdsrlb ip l show eth0 | grep "link/ether" | awk '{print $2}')
 
-    web1:
-      kind: linux
-      image: 192.168.2.100:5000/nettool
-      exec:
-      - ip addr add 10.1.5.200/24 dev net0
-      - ip route replace default via 10.1.5.1
+docker exec -it xdpdsrlb bash
+cd XDP_LBDSR*
+./xdp_lbdsr
 
-    web2:
-      kind: linux
-      image: 192.168.2.100:5000/nettool
-      exec:
-      - ip addr add 10.1.5.201/24 dev net0
-      - ip route replace default via 10.1.5.1
-      
-    client:
-      kind: linux
-      image: 192.168.2.100:5000/xcni
-      exec:
-      - ip addr add 10.1.5.5/24 dev net0
-      - ip route replace default via 10.1.5.1
-      - sh -c 'echo "10.1.5.100 www.wluo.com" >> /etc/hosts'
+while true;do curl -s http://192.168.25.10;sleep 1;done
 
-  links:
-    - endpoints: ["nginx1:net0", "brl4l7:net1"]
-    - endpoints: ["nginx2:net0", "brl4l7:net2"]
-    - endpoints: ["keepalived1-haproxy1:net1", "brl4l7:net3"]
-    - endpoints: ["keepalived2-haproxy2:net1", "brl4l7:net4"]
-    - endpoints: ["client:net0", "brl4l7:net5"]
-    - endpoints: ["web1:net0", "brl4l7:net6"]
-    - endpoints: ["web2:net0", "brl4l7:net7"]
-EOF
-
+kill -9 $(pidof tail)
