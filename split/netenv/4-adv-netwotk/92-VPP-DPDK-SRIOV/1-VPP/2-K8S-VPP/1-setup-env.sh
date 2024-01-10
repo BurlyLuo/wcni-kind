2d45e246ea79387bcee8d81e667c0283f4ae41a2
rowan <olaf.luo@foxmail.com>
2024-01-10 10:39:11 +0800

    defails about the vpp running at k8s pod

--- split/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV/1-VPP/2-K8S-VPP/1-setup-env.sh
+++ split/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV/1-VPP/2-K8S-VPP/1-setup-env.sh
@@ -1,6 +1,5 @@
 # 0.env perp:
 virt-install --name vpp1 --memory 10240  --cpu host-model --vcpus=8 --disk /root/kvm/vpp1.qcow2,device=disk,bus=virtio --disk size=30 --os-variant centos7.0 --virt-type kvm --graphics none --network=bridge=brnet,model=virtio --network=bridge=vppdpdk5,model=virtio --network=bridge=vppdpdk8,model=virtio --import
-
 sshpass -p hive ssh-copy-id -o StrictHostKeyChecking=no -p 22 root@192.168.2.98 > /dev/null 2>&1
 master_ip=192.168.2.98
 k3sup install --ip=$master_ip --user=root --merge --sudo --cluster --k3s-version=v1.27.3+k3s1 --k3s-extra-args "--flannel-backend=none --cluster-cidr=10.244.0.0/16 --disable-network-policy --disable traefik --disable servicelb --node-ip=$master_ip" --local-path $HOME/.kube/config --context=vpp
@@ -8,24 +7,49 @@ kubectl create -f https://raw.githubusercontent.com/flannel-io/flannel/master/Do
 
 # 1.hugepage and cpu isolution:
 vi /etc/default/grub 
-GRUB_CMDLINE_LINUX="console=tty0 crashkernel=auto net.ifnames=0 console=ttyS0 isolcpus=1,2,3  default_hugepagesz=1G hugepagesz=1G hugepages=2"
+GRUB_CMDLINE_LINUX="console=tty0 crashkernel=auto net.ifnames=0 console=ttyS0 isolcpus=1,2,3 iommu=pt intel_iommu=on default_hugepagesz=1G hugepagesz=1G hugepages=2"
 grub2-mkconfig -o /boot/grub2/grub.cfg && reboot
-if abnormal with hugepage, pls mode the hugepage, and restart k3s
+
+if abnormal with hugepage, pls mode the hugepage, and restart k3s[systemctl restart k3s]
 [root@localhost hugepages-1048576kB]# pwd
 /sys/devices/system/node/node0/hugepages/hugepages-1048576kB
 [root@localhost hugepages-1048576kB]# ll
-total 0
 -r--r--r--. 1 root root 4096 Jan  7 05:27 free_hugepages
--rw-r--r--. 1 root root 4096 Jan  7 05:27 nr_hugepages
+-rw-r--r--. 1 root root 4096 Jan  7 05:27 nr_hugepages [echo 2 > nr_hugepages] [systemctl restart k3s]
 -r--r--r--. 1 root root 4096 Jan  7 05:27 surplus_hugepages
 [root@localhost hugepages-1048576kB]# 
+#[outputs:]
+[Capacity:
+  cpu:                       8
+  ephemeral-storage:         52416384Ki
+  hugepages-1Gi:             2Gi   
+  intel.com/sriov_vppdpdk5:  1
+  intel.com/sriov_vppdpdk8:  1
+  memory:                    10072208Ki
+  pods:                      110
+Allocatable:
+  cpu:                       8
+  ephemeral-storage:         50990658316
+  hugepages-1Gi:             2Gi
+  intel.com/sriov_vppdpdk5:  1
+  intel.com/sriov_vppdpdk8:  1
+  memory:                    7975056Ki
+  pods:                      110]
 
 # 2.multus mutiple nics:
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick.yml
- 
+
 # 3.sriov-dpdk resource
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/sriov-network-device-plugin/master/deployments/sriovdp-daemonset.yaml
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/sriov-cni/master/images/sriov-cni-daemonset.yaml
+ip l s eth1 down && ip l s eth2 down
+lshw -class network -businfo
+modprobe vfio_pci
+echo 1 > /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
+./dpdk.py --bind=vfio-pci 0000:02:00.0
+./dpdk.py --bind=vfio-pci 0000:03:00.0
+
+# vpp.yaml
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: ConfigMap
@@ -56,10 +80,40 @@ data:
     }
 EOF
 
+#[outputs]
+Capacity:
+  cpu:                       8
+  ephemeral-storage:         52416384Ki
+  hugepages-1Gi:             2Gi
+  intel.com/sriov_vppdpdk5:  1
+  intel.com/sriov_vppdpdk8:  1
+  memory:                    10072208Ki
+  pods:                      110
+Allocatable:
+  cpu:                       8
+  ephemeral-storage:         50990658316
+  hugepages-1Gi:             2Gi
+  intel.com/sriov_vppdpdk5:  1
+  intel.com/sriov_vppdpdk8:  1
+  memory:                    7975056Ki
+  pods:                      110
+
 # 4.whereabouts IPAM
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/whereabouts/master/doc/crds/daemonset-install.yaml
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/whereabouts/master/doc/crds/whereabouts.cni.cncf.io_ippools.yaml
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/whereabouts/master/doc/crds/whereabouts.cni.cncf.io_overlappingrangeipreservations.yaml
+#[All mutiple nics:]
+[root@localhost ~]# kubectl get pods -owide -A
+NAMESPACE      NAME                                     READY   STATUS    RESTARTS       AGE     IP             NODE        NOMINATED NODE   READINESS GATES
+kube-flannel   kube-flannel-ds-8n5nf                    1/1     Running   17 (20h ago)   2d19h   192.168.2.98   localhost   <none>           <none>
+kube-system    coredns-77ccd57875-25zzx                 1/1     Running   10 (20h ago)   2d19h   10.42.0.50     localhost   <none>           <none>
+kube-system    kube-multus-ds-jnkgd                     1/1     Running   4 (20h ago)    2d18h   192.168.2.98   localhost   <none>           <none>
+kube-system    kube-sriov-cni-ds-amd64-bbndx            1/1     Running   10 (20h ago)   2d19h   10.42.0.49     localhost   <none>           <none>
+kube-system    kube-sriov-device-plugin-amd64-wwtbz     1/1     Running   4 (20h ago)    2d18h   192.168.2.98   localhost   <none>           <none>
+kube-system    local-path-provisioner-957fdf8bc-b7wh8   1/1     Running   16 (20h ago)   2d19h   10.42.0.47     localhost   <none>           <none>
+kube-system    metrics-server-648b5df564-dn2pd          1/1     Running   15 (20h ago)   2d19h   10.42.0.48     localhost   <none>           <none>
+kube-system    whereabouts-dxmp9                        1/1     Running   12 (20h ago)   2d19h   192.168.2.98   localhost   <none>           <none>
+[root@localhost ~]# 
 
 # 5.vpp.yaml
 cat <<EOF | kubectl apply -f -
@@ -72,13 +126,16 @@ metadata:
 spec:
   containers:
   - name: vpp
-    image: centos/tools 
-    #image: 192.168.2.100:5000/centos/tools
+    image: soelvkaer/vppcontainer:latest
     imagePullPolicy: IfNotPresent
     volumeMounts:
     - name: hugepage
       mountPath: /hugepages
-    command: [ "/sbin/init" ]
+    - name: vppconfig
+      mountPath: /etc/vpp/
+    - name: startupconfig
+      mountPath: /root/
+    #command: [ "/sbin/init" ]
     #command: [ "/bin/bash", "-c", "--" ]
     #args: [ "while true; do sleep 300000; done;" ]
     securityContext:
@@ -100,8 +157,59 @@ spec:
   - name: hugepage
     emptyDir:
       medium: HugePages
+  - name: vppconfig
+    configMap:
+      name: vppconfig
+  - name: startupconfig
+    configMap:
+      name: startupconfig
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: vppconfig
+data:
+  startup.conf: |
+    unix {
+      nodaemon
+      log /var/log/vpp/vpp.log
+      full-coredump
+      cli-listen /run/vpp/cli.sock
+      gid vpp
+      startup-config /root/startupconfig
+    }
+    api-trace {
+      on
+    }
+    api-segment {
+      gid vpp
+    }
+    cpu { 
+        main-core 0
+        corelist-workers 1,2
+    }
+    dpdk {
+      dev 0000:02:00.0 {
+          name fpeth1
+      }
+      dev 0000:03:00.0 {
+          name fpeth2
+    }
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: startupconfig
+data:
+  startupconfig: |
+    set interface ip address fpeth1 10.1.5.10/24
+    set interface state fpeth1 up
+    set interface ip address fpeth2 10.1.8.10/24
+    set interface state fpeth2 up
+    ip route add 0.0.0.0/0 via 10.1.5.254 fpeth1
 EOF
 
+[if with centos/tools, need install vpp manually.]
 #6. install vpp at vpp_pod
 # kubectl exec -it vpp bash
 yum -y install epel-release && yum list && yum update -y
