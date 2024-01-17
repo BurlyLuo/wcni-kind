ba99eb5fd7aa76c243cf015f55e0f1f904f3738b
rowan <olaf.luo@foxmail.com>
2024-01-17 17:52:37 +0800

    add lcpng mode SBR demo

--- split/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/03-IPng-VPP-LCP/1-setup-env.sh
+++ split/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/03-IPng-VPP-LCP/1-setup-env.sh
@@ -41,3 +41,8 @@ ip r a 114.114.114.114 via 10.1.5.254 dev fpeth1
 # switch back to vpp side:
 vppctl show ip fib | grep 114.114.114.114 -C 5
 
+# SBR:
+ip route add 0.0.0.0/0 via 10.1.5.254 table 100
+ip rule add from 10.1.5.0/24 table 100
+
+
