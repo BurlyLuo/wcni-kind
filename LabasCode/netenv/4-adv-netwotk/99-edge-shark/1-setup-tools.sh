2e11774381e86dc2b27339b67150f165e58497f8
rowan <olaf.luo@foxmail.com>
2024-02-18 14:43:42 +0800

    add edgeshark for KinD env and containerlab environment

---
+++ LabasCode/netenv/4-adv-netwotk/99-edge-shark/1-setup-tools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 1. Install edgeshark
+wget -q --no-cache -O - \
+  https://github.com/siemens/edgeshark/raw/main/deployments/nocomposer/edgeshark.sh | bash -s up
+
+# $ docker ps -a 
+# 061a267b7856   ghcr.io/siemens/packetflix   "/packetflix --port=…"   2 hours ago         Up 2 hours         5000/tcp, 0.0.0.0:5001->5001/tcp, :::5001->5001/tcp   edgeshark
+# dbdfa3430226   ghcr.io/siemens/ghostwire    "/gostwire --http=[:…"   2 hours ago         Up 2 hours                                                               gostwire
+
+# Open edgeshark webui
+# firefox http://192.168.2.99:5001
+
+# 2. Install Containershark Extcap Plugin for Wireshark
+cshargextcap=cshargextcap_0.10.7_linux_amd64.deb && wget https://github.com/siemens/cshargextcap/releases/download/v0.10.7/$cshargextcap && dpkg -i ./$cshargextcap  && rm -rf ./$cshargextcap
