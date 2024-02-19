6459fdca520ba262b795a9de54e0f176acc3c20e
rowan <olaf.luo@foxmail.com>
2024-02-19 15:11:49 +0800

    add local cilium image load

--- LabasCode/cilium/cilium-1.15.0-rc.1/cilium-kubeproxy/1-direct-routing/1-setup-env.sh
+++ LabasCode/cilium/cilium-1.15.0-rc.1/cilium-kubeproxy/1-direct-routing/1-setup-env.sh
@@ -23,11 +23,13 @@ kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.
 kubectl get nodes -o wide
 
 # 3. Install CNI[Cilium 1.15.0-rc.1]
-helm repo add cilium https://helm.cilium.io > /dev/null 2>&1
-helm repo update > /dev/null 2>&1
+cilium_version=v1.15.0-rc.1
+docker pull quay.io/cilium/cilium:$cilium_version && docker pull quay.io/cilium/operator-generic:$cilium_version
+kind load docker-image quay.io/cilium/cilium:$cilium_version quay.io/cilium/operator-generic:$cilium_version --name cilium-kubeproxy
+{ helm repo add cilium https://helm.cilium.io ; helm repo update; } > /dev/null 2>&1
 
 # Direct Routing Options(--set routingMode=native --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8")
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.15.0-rc.1 --namespace kube-system --set debug.enabled=true --set debug.verbose="datapath flow kvstore envoy policy" --set bpf.monitorAggregation=none --set monitor.enabled=true --set ipam.mode=cluster-pool --set cluster.name=cilium-kubeproxy --set routingMode=native --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8"
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.15.0-rc.1 --namespace kube-system --set image.pullPolicy=IfNotPresent --set debug.enabled=true --set debug.verbose="datapath flow kvstore envoy policy" --set bpf.monitorAggregation=none --set monitor.enabled=true --set ipam.mode=cluster-pool --set cluster.name=cilium-kubeproxy --set routingMode=native --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8"
 
 # 4. Wait all pods ready
 kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
