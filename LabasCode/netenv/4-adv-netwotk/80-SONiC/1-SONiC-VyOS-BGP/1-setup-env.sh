523dc80ff8d905e42dca0d16cc55fbad58d90046
rowan <olaf.luo@foxmail.com>
2024-02-28 17:25:37 +0800

    x sonic example

--- LabasCode/netenv/4-adv-netwotk/80-SONiC/1-SONiC-VyOS-BGP/1-setup-env.sh
+++ LabasCode/netenv/4-adv-netwotk/80-SONiC/1-SONiC-VyOS-BGP/1-setup-env.sh
@@ -10,18 +10,11 @@ topology:
         - /lib/modules:/lib/modules
         - ./startup-conf/vyos.cfg:/opt/vyatta/etc/config/config.boot 
     sonic:
-      kind: sonic-vs
+      kind: linux
       image: 192.168.2.100:5000/docker-sonic-vs:2020-11-12
       binds:
         - ./startup-conf/sonic.cfg:/etc/frr/frr.conf
-      #exec:
-      #  - >
-      #    bash -c '
-      #    config interface ip add Ethernet0 10.1.5.11/24 &&
-      #    config interface startup Ethernet0 &&
-      #    config loopback add Loopback0 &&
-      #    config interface ip add Loopback0 2.2.2.2/24 &&
-      #    config interface startup Loopback0'
+        - ./startup-conf/daemons:/etc/frr/daemons
 
   links:
     - endpoints: ["vyos:eth1", "sonic:eth1"]
@@ -30,3 +23,7 @@ EOF
 sleep 5
 
 docker exec -it clab-sonic-sonic bash -c "config interface ip add Ethernet0 10.1.5.11/24 && config interface startup Ethernet0 && config loopback add Loopback0 && config interface ip add Loopback0 2.2.2.2/24 && config interface startup Loopback0"
+
+sleep 5
+docker exec -it clab-sonic-sonic bash -c "ip l s eth1 up && service frr restart >/dev/null 2>&1 && service frr restart"
+
