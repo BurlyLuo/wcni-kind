8625845e90a5391b192fc5bf920a2a5739183a33
rowan <olaf.luo@foxmail.com>
2024-03-16 08:01:50 +0800

    Adjust/Rename the Directory/File

--- LabasCode/k8senv/vmenv/8-setup-env-22.04-k8s.sh
+++ LabasCode/k8senv/vmenv/8-setup-env-22.04-k8s.sh
@@ -37,4 +37,3 @@ for ((ip_id=0; ip_id<${#ip_addresses[@]}; ip_id++)); do
     master_ip=${ip_addresses[$ip_id]}
     k3sup install --ip=$master_ip --user=root --merge --sudo --cluster --k3s-version=v1.27.3+k3s1 --k3s-extra-args "--flannel-backend=none --cluster-cidr=10.244.0.0/16 --disable-network-policy --disable traefik --disable servicelb --node-ip=$master_ip" --local-path $HOME/.kube/config --context=2204ctx
 done
-
