83ddef29dabe25e69476c57239e2cd02da4bdb71
rowan <olaf.luo@foxmail.com>
2024-03-23 09:27:26 +0800

    full and fix cilium-Socket-LoadBalancer

--- LabasCode/cilium/cilium-1.15.0-rc.1/cilium-Kubernetes-Networking/3-Kubernetes-Without-kube-proxy/5-cilium-Socket-LoadBalancer/1-setup-env.sh
+++ LabasCode/cilium/cilium-1.15.0-rc.1/cilium-Kubernetes-Networking/3-Kubernetes-Without-kube-proxy/5-cilium-Socket-LoadBalancer/1-setup-env.sh
@@ -80,3 +80,4 @@ kubectl -nkube-system exec -it ds/cilium -- cilium status
 for container in $(docker ps -a --format "table {{.Names}}" | grep cilium-socket-lb);do docker exec $container ls -al /proc/self/ns/cgroup;done
 mount -l | grep cgroup && docker info | grep "Cgroup Version" | awk '$1=$1'
 
+kubectl apply -f cni.yaml
