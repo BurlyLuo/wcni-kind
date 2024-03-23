01ff37ea8867d963b8a98d9d3b8cf6abdd0014dc
rowan <olaf.luo@foxmail.com>
2024-03-23 09:27:26 +0800

    full and fix cilium-Socket-LoadBalancer

---
+++ LabasCode/cilium/cilium-1.15.0-rc.1/cilium-Kubernetes-Networking/3-Kubernetes-Without-kube-proxy/5-cilium-Socket-LoadBalancer/3-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
+controller_node_name=`kubectl get nodes -o wide | grep control-plane | awk -F " " '{print $1}'`
+
+test_pod_name=$(kubectl get pods -owide | grep $controller_node_name | awk -F " " '{print $1}') && echo PodName: $test_pod_name
+test_pod_ip=$(kubectl get pods -owide | grep $controller_node_name | awk -F " " '{print $6}') && echo PodIP: $test_pod_ip
+
+# make a test:
+echo "kubectl exec -t $test_pod_name -- tcpdump -pne -i eth0 &"
+kubectl exec -t $test_pod_name -- tcpdump -pne -i eth0 &
+sleep 1
+echo "********************************************************************"
+echo "kubectl exec -it $test_pod_name -- curl $controller_node_ip:32000"
+kubectl exec -it $test_pod_name -- curl $controller_node_ip:32000
+echo "********************************************************************"
+sleep 2
+kubectl exec -t $test_pod_name -- killall tcpdump
