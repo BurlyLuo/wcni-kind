43557e3270ffb8475231483c0f1000eb9af151e1
rowan <olaf.luo@foxmail.com>
2024-03-28 18:45:47 +0800

    demo cilium kubeproxy mode

--- LabasCode/cilium/cilium-1.15.0-rc.1/cilium-eBPF-DataPath/2-cilium-KubeProxy-NativeRouting/1-setup-env.sh
+++ LabasCode/cilium/cilium-1.15.0-rc.1/cilium-eBPF-DataPath/2-cilium-KubeProxy-NativeRouting/1-setup-env.sh
@@ -10,7 +10,6 @@ networking:
 nodes:
   - role: control-plane
   - role: worker
-  - role: worker
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."192.168.2.100:5000"]
@@ -29,7 +28,7 @@ kind load docker-image quay.io/cilium/cilium:$cilium_version quay.io/cilium/oper
 { helm repo add cilium https://helm.cilium.io ; helm repo update; } > /dev/null 2>&1
 
 # Direct Routing Options(--set routingMode=native --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8")
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.15.0-rc.1 --namespace kube-system --set image.pullPolicy=IfNotPresent --set debug.enabled=true --set debug.verbose="datapath flow kvstore envoy policy" --set bpf.monitorAggregation=none --set monitor.enabled=true --set ipam.mode=cluster-pool --set cluster.name=cilium-kubeproxy --set routingMode=native --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8"
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.15.0-rc.1 --namespace kube-system --set image.pullPolicy=IfNotPresent --set debug.enabled=true --set debug.verbose="datapath flow kvstore envoy policy" --set bpf.monitorAggregation=none --set monitor.enabled=true --set ipam.mode=kubernetes --set cluster.name=cilium-kubeproxy --set routingMode=native --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8"
 
 # 4. Wait all pods ready
 kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
@@ -42,5 +41,4 @@ for container in $(docker ps -a --format "table {{.Names}}" | grep cilium-kubepr
 mount -l | grep cgroup && docker info | grep "Cgroup Version" | awk '$1=$1'
 
 # 7. Create Pod & Service
-kubectl apply -f cni.yaml
-
+./2-test.sh
