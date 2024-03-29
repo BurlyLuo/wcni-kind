5ac84897785f28bad502ad830f91438d4569bad9
rowan <olaf.luo@foxmail.com>
2024-03-29 14:15:41 +0800

    Adjust/Rename the Directory/File

--- LabasCode/cilium/cilium-1.15.0-rc.1/cilium-eBPF-DataPath/2-cilium-KubeProxy-NativeRouting/4-service_datapath/1-setup-env-multipass.sh
+++ LabasCode/cilium/cilium-1.15.0-rc.1/cilium-eBPF-DataPath/2-cilium-KubeProxy-NativeRouting/4-service_datapath/1-setup-env-multipass.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 set -v
-
 # 1. Deploy multipass vmn(kubeProxyReplacement=false(default))
 for ((i=0; i<${1:-3}; i++))
 do
@@ -34,4 +33,21 @@ for ((ip_id=0; ip_id<${#ip_addresses[@]}; ip_id++)); do
         k3sup join --ip ${ip_addresses[$ip_id]} --user root --sudo --k3s-version=$k3s_version --server-ip $master_ip --server-user root
     fi
 done
+
+# 3. Install Cilium CNI
+{ helm repo add cilium https://helm.cilium.io ; helm repo update; } > /dev/null 2>&1
+controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
+kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/control-plane:NoSchedule-
+
+# Direct Routing Options(--set routingMode=native --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8")
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.15.0-rc.1 --namespace kube-system --set image.pullPolicy=IfNotPresent --set debug.enabled=true --set debug.verbose="datapath flow kvstore envoy policy" --set bpf.monitorAggregation=none --set monitor.enabled=true --set ipam.mode=kubernetes --set cluster.name=cilium-kubeproxy --set routingMode=native --set autoDirectNodeRoutes=true --set ipv4NativeRoutingCIDR="10.0.0.0/8"
+
+# 4. Wait all pods ready
+kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
+
+# 5. Cilium status(cilium status --verbose)
+kubectl -nkube-system exec -it ds/cilium -- cilium status
+
+# 6. Create Pod & Service
+./2-test.sh
 
