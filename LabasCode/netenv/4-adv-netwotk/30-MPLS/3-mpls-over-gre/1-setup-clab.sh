ea5034f2d22cd79eda2b667dbf89f09e6ba67ec3
rowan <olaf.luo@foxmail.com>
2024-06-03 14:43:55 +0800

    add mpls 2nodes and 3nodes

--- LabasCode/netenv/4-adv-netwotk/30-MPLS/3-mpls-over-gre/1-setup-clab.sh
+++ LabasCode/netenv/4-adv-netwotk/30-MPLS/3-mpls-over-gre/1-setup-clab.sh
@@ -4,9 +4,9 @@ set -v
 # GRE:
 #tunnel_ip:1.1.1.1/24 tunnel_ip:1.1.1.2/24
 #    172.12.1.10/.11   172.23.1.10/.11
-#   gre1    ---    gre2    ---    gre3 
+#   gre1    ---    ---    ---    gre3 
 #   /                               \
-#10.1.5.10                        10.1.8.10
+#2.2.2.2                         3.3.3.3
 # tunnel_ip: 仅仅是配置地址，但是实际上用不上，在配置路由的时候指定的还是接口：set protocols static route 10.1.8.0/24 interface tun0
 
 # MPLS with GRE
@@ -32,35 +32,7 @@ topology:
         - /lib/modules:/lib/modules
         - ./startup-conf/mpls-gre2.cfg:/opt/vyatta/etc/config/config.boot
 
-    r3:
-      kind: linux
-      image: 192.168.2.100:5000/vyos/vyos:1.4.9
-      cmd: /sbin/init
-      binds:
-        - /lib/modules:/lib/modules
-        - ./startup-conf/mpls-gre3.cfg:/opt/vyatta/etc/config/config.boot
-
-    server1:
-      kind: linux
-      image: 192.168.2.100:5000/nettool
-      exec:
-      - ip addr add 10.1.5.10/24 dev net0
-      - ip route replace default via 10.1.5.1
-
-    server2:
-      kind: linux
-      image: 192.168.2.100:5000/nettool
-      exec:
-      - ip addr add 10.1.8.10/24 dev net0
-      - ip route replace default via 10.1.8.1
-
-
   links:
-    - endpoints: ["r1:eth1", "server1:net0"]
-    - endpoints: ["r3:eth1", "server2:net0"]
-
-    - endpoints: ["r1:eth2", "r2:eth1"]
-    - endpoints: ["r2:eth2", "r3:eth2"]
+    - endpoints: ["r1:eth1", "r2:eth1"]
 
 EOF
-
