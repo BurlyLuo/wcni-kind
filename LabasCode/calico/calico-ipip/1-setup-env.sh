7bef3a31e8fa54816308a5ca11b99836d6067f8f
rowan <olaf.luo@foxmail.com>
2024-07-22 17:22:00 +0800

    Add: add zone anti affinitity

--- LabasCode/calico/calico-ipip/1-setup-env.sh
+++ LabasCode/calico/calico-ipip/1-setup-env.sh
@@ -15,6 +15,9 @@ containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."192.168.2.100:5000"]
     endpoint = ["http://192.168.2.100:5000"]
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."https://dockerproxy.com"]
+    endpoint = ["https://dockerproxy.com"]
 EOF
 
 # 2.remove taints
