035c45ecdcbd4fe7f3b5643f352fca43c44e37b4
rowan <olaf.luo@foxmail.com>
2024-08-17 15:26:13 +0800

    Fix: Add compare for VRF

--- LabasCode/netenv/4-adv-netwotk/29-VRF/2-vrf-vyos/1-setup-clab.sh
+++ LabasCode/netenv/4-adv-netwotk/29-VRF/2-vrf-vyos/1-setup-clab.sh
@@ -19,12 +19,14 @@ topology:
         - ./startup-conf/gwx.cfg:/opt/vyatta/etc/config/config.boot
 
     server1:
+      # VRF 5
       kind: linux
       image: 192.168.2.100:5000/nettool
       exec:
       - ip addr add 10.1.5.10/24 dev net0
       - ip route replace default via 10.1.5.1
     server2:
+      # VRF 5
       kind: linux
       image: 192.168.2.100:5000/nettool
       exec:
@@ -32,18 +34,27 @@ topology:
       - ip route replace default via 10.1.5.1
 
     server3:
+      # VFR 8
       kind: linux
       image: 192.168.2.100:5000/nettool
       exec:
       - ip addr add 10.1.8.10/24 dev net0
       - ip route replace default via 10.1.8.1
 
+    server4:
+      # No VRF(default vrf)
+      kind: linux
+      image: 192.168.2.100:5000/nettool
+      exec:
+      - ip addr add 10.1.9.10/24 dev net0
+      - ip route replace default via 10.1.9.1
 
   links:
     - endpoints: ["gwx:eth1", "br-vrf:eth1"]
     - endpoints: ["br-vrf:eth2", "server1:net0"]
     - endpoints: ["br-vrf:eth3", "server2:net0"]
     - endpoints: ["gwx:eth2", "server3:net0"]
+    - endpoints: ["gwx:eth3", "server4:net0"]
 
 EOF
 
