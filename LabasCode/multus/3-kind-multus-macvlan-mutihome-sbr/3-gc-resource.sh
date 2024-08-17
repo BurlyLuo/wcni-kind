21b083216270231c99938fae215570392773019e
rowan <olaf.luo@foxmail.com>
2024-08-17 20:04:01 +0800

    Fix: unify case

--- LabasCode/multus/3-kind-multus-macvlan-mutihome-sbr/3-gc-resource.sh
+++ LabasCode/multus/3-kind-multus-macvlan-mutihome-sbr/3-gc-resource.sh
@@ -2,4 +2,4 @@
 set -v
 kubectl delete pods multihoming-pod1 multihoming-pod2
 sleep 3
-kubectl delete net-attach-def multihoming-muti-nic-sbr-conf1 multihoming-muti-nic-sbr-conf2
+kubectl delete net-attach-def multihoming-sbr-conf1 multihoming-sbr-conf2
