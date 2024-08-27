0fa6529c59b4d8b7fec84928a63d80bfd7faf6ab
rowan <olaf.luo@foxmail.com>
2024-08-27 14:01:37 +0800

    Add: add afxdp plugin

--- LabasCode/multus/13-kind-multus-af-xdp/1-setup-env.sh
+++ LabasCode/multus/13-kind-multus-af-xdp/1-setup-env.sh
@@ -3,44 +3,18 @@ date
 set -v
 
 # 1.prep noCNI env
-cat <<EOF | kind create cluster --name=cni-multus --image=kindest/node:v1.27.3 --config=-
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-networking:
-        disableDefaultCNI: true
-nodes:
-        - role: control-plane
-        - role: worker
-          extraMounts:
-            - hostPath: /tmp/afxdp_dp/
-              containerPath: /tmp/afxdp_dp/
-              propagation: Bidirectional
-              selinuxRelabel: false
-        - role: worker
-          extraMounts:
-            - hostPath: /tmp/afxdp_dp2/
-              containerPath: /tmp/afxdp_dp/
-              propagation: Bidirectional
-              selinuxRelabel: false
-
-containerdConfigPatches:
-- |-
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."192.168.2.100:5000"]
-    endpoint = ["http://192.168.2.100:5000"]
-EOF
+alias addk3s
+addk3s
 
 # 2.remove taints
 controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
 kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl get nodes -o wide
-kubectl label node cni-multus-worker cndp="true"
-kubectl label node cni-multus-worker2 cndp="true"
 
-./2-setup-clab.sh
 
-for i in $(docker ps --format '{{.Names}}'| grep cni-multus- | grep -v clab);do docker exec -it $i bash -c "sysctl kernel.unprivileged_bpf_disabled=0";done
+# for i in $(docker ps --format '{{.Names}}'| grep cni-multus- | grep -v clab);do docker exec -it $i bash -c "sysctl kernel.unprivileged_bpf_disabled=0";done
 
-# 3. install CNI[Calico v3.23.2]
+# 3. install CNI[Calico v3.23.2] and afxdp-plugin
 kubectl apply -f ./k8snetworkplumbingwg
 
 # 4. wait all pods ready
