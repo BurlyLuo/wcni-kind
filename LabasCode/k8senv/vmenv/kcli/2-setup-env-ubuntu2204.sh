6c65cf978f30658e4e8622b5f462e6c906ab075a
rowan <olaf.luo@foxmail.com>
2024-09-09 13:42:53 +0800

    Adjust/Rename the Directory/File

--- LabasCode/k8senv/vmenv/kcli/2-setup-env-ubuntu2204.sh
+++ LabasCode/k8senv/vmenv/kcli/2-setup-env-ubuntu2204.sh
@@ -9,6 +9,6 @@ for vm in {0,1,2}; do
     -P rootpassword=hive \
     -P nets="[{'name':'brnet','ip':'$ip','netmask':'24','gateway':'192.168.2.1'},{'name':'vppdpdk5'},{'name':'vppdpdk8'},{'name':'vppdpdk9'}]" \
     -P cmds='["apt -y install net-tools pciutils wget lrzsz" , "wget http://192.168.2.100/kvm/tools/lseth -P /usr/bin/ && chmod +x /usr/bin/lseth"]' \
-    vm$vm
+    uvm$vm
 done
 
