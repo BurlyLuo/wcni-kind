44ba1410f1439c55c7fe40e514e88c1462a41792
rowan <olaf.luo@foxmail.com>
2024-09-25 14:57:11 +0800

    Add a demo for kcli

--- LabasCode/k8senv/vmenv/virtkcli/x-kinds.sh
+++ LabasCode/k8senv/vmenv/virtkcli/x-kinds.sh
@@ -1,3 +1,5 @@
+# kcli render -f ./all_parameters.yml -c
+
 # 1. centos7
 kcli create vm -i       centos7 -P memory=4096 -P disks=[50] -P rootpassword=hive -P nets="[{'name':'brnet','ip':'192.168.2.96','netmask':'24','gateway':'192.168.2.1'},{'name':'vppdpdk5'},{'name':'vppdpdk8'},{'name':'vppdpdk9'}]" -P cpupinning=['{"vcpus": "0", "hostcpus": "0"}','{"vcpus": "1", "hostcpus": "1"}','{"vcpus": "2", "hostcpus": "2"}','{"vcpus": 3, "hostcpus": 3}'] -P numcpus=4 -P cmds='[rm -rf /etc/yum.repos.d/* && curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo && yum -y install net-tools pciutils wget lrzsz && wget http://192.168.2.100/kvm/tools/lseth -P /usr/bin/ && chmod +x /usr/bin/lseth]' vm
 
