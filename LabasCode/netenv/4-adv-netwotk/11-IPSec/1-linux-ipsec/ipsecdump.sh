0f5f700cd76b4343a95191bf00a3835387f65ca8
rowan <olaf.luo@foxmail.com>
2024-10-15 21:12:20 +0800

    Mod ipsecdump.sh

--- LabasCode/netenv/4-adv-netwotk/11-IPSec/1-linux-ipsec/ipsecdump.sh
+++ LabasCode/netenv/4-adv-netwotk/11-IPSec/1-linux-ipsec/ipsecdump.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget http://192.168.2.100/k3s/go/go1.23.2.linux-amd64.tar.gz -P / && tar -C /usr/local -xzf /go1.23.2.linux-amd64.tar.gz
 export GOPATH=$HOME/go && export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
 go env -w GO111MODULE=on && go env -w GOPROXY=https://goproxy.cn,direct
