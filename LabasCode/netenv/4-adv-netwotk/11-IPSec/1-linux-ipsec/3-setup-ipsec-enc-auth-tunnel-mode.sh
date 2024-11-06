c00e3e04295e18074bdfc167790b871eac2e4220
rowan <olaf.luo@foxmail.com>
2024-11-06 16:49:28 +0800

    Fix ipsec tunnel mode

--- LabasCode/netenv/4-adv-netwotk/11-IPSec/1-linux-ipsec/3-setup-ipsec-enc-auth-tunnel-mode.sh
+++ LabasCode/netenv/4-adv-netwotk/11-IPSec/1-linux-ipsec/3-setup-ipsec-enc-auth-tunnel-mode.sh
@@ -10,7 +10,6 @@ topology:
       exec:
       - ip a a 10.1.5.1/24 dev net1
       - ip a a 10.1.8.1/24 dev net2
-      - ip a a 10.1.9.1/24 dev net3
 
 
     ipsec1:
@@ -25,16 +24,9 @@ topology:
       - ip xfrm state add src 10.1.5.10 dst 10.1.8.10 proto esp spi 0xfe51d977 reqid 0xfe51d977 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
       - ip xfrm state add src 10.1.8.10 dst 10.1.5.10 proto esp spi 0xfe51d977 reqid 0xfe51d977 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
 
-      - ip xfrm state add src 10.1.5.10 dst 10.1.9.10 proto esp spi 0xfe51d978 reqid 0xfe51d978 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
-      - ip xfrm state add src 10.1.9.10 dst 10.1.5.10 proto esp spi 0xfe51d978 reqid 0xfe51d978 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
-
       - ip xfrm policy add src 10.244.1.10/24 dst 10.244.2.10/24 dir out tmpl src 10.1.5.10 dst 10.1.8.10 proto esp reqid 0xfe51d977 mode tunnel
-      - ip xfrm policy add src 10.244.1.10/24 dst 10.244.2.10/24 dir fwd tmpl src 10.1.8.10 dst 10.1.5.10 proto esp reqid 0xfe51d977 mode tunnel
+      - ip xfrm policy add src 10.244.2.10/24 dst 10.244.1.10/24 dir fwd tmpl src 10.1.8.10 dst 10.1.5.10 proto esp reqid 0xfe51d977 mode tunnel
       - ip xfrm policy add src 10.244.2.10/24 dst 10.244.1.10/24 dir in  tmpl src 10.1.8.10 dst 10.1.5.10 proto esp reqid 0xfe51d977 mode tunnel
-
-      - ip xfrm policy add src 10.244.1.10/24 dst 10.244.3.10/24 dir out tmpl src 10.1.5.10 dst 10.1.9.10 proto esp reqid 0xfe51d978 mode tunnel
-      - ip xfrm policy add src 10.244.1.10/24 dst 10.244.3.10/24 dir fwd tmpl src 10.1.9.10 dst 10.1.5.10 proto esp reqid 0xfe51d978 mode tunnel
-      - ip xfrm policy add src 10.244.3.10/24 dst 10.244.1.10/24 dir in  tmpl src 10.1.9.10 dst 10.1.5.10 proto esp reqid 0xfe51d978 mode tunnel
 
       - iptables -t nat -A POSTROUTING -s 10.1.0.0/16 -o eth0 -j MASQUERADE
 
@@ -53,45 +45,9 @@ topology:
       - ip xfrm state add src 10.1.8.10 dst 10.1.5.10 proto esp spi 0xfe51d977 reqid 0xfe51d977 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
       - ip xfrm state add src 10.1.5.10 dst 10.1.8.10 proto esp spi 0xfe51d977 reqid 0xfe51d977 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
 
-      - ip xfrm state add src 10.1.8.10 dst 10.1.9.10 proto esp spi 0xfe51d979 reqid 0xfe51d979 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
-      - ip xfrm state add src 10.1.9.10 dst 10.1.8.10 proto esp spi 0xfe51d979 reqid 0xfe51d979 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
-
       - ip xfrm policy add src 10.244.2.10/24 dst 10.244.1.10/24 dir out tmpl src 10.1.8.10 dst 10.1.5.10 proto esp reqid 0xfe51d977 mode tunnel
-      - ip xfrm policy add src 10.244.2.10/24 dst 10.244.1.10/24 dir fwd tmpl src 10.1.5.10 dst 10.1.8.10 proto esp reqid 0xfe51d977 mode tunnel
+      - ip xfrm policy add src 10.244.1.10/24 dst 10.244.2.10/24 dir fwd tmpl src 10.1.5.10 dst 10.1.8.10 proto esp reqid 0xfe51d977 mode tunnel
       - ip xfrm policy add src 10.244.1.10/24 dst 10.244.2.10/24 dir in  tmpl src 10.1.5.10 dst 10.1.8.10 proto esp reqid 0xfe51d977 mode tunnel
-
-      - ip xfrm policy add src 10.244.2.10/24 dst 10.244.3.10/24 dir out tmpl src 10.1.8.10 dst 10.1.9.10 proto esp reqid 0xfe51d979 mode tunnel
-      - ip xfrm policy add src 10.244.2.10/24 dst 10.244.3.10/24 dir fwd tmpl src 10.1.9.10 dst 10.1.8.10 proto esp reqid 0xfe51d979 mode tunnel
-      - ip xfrm policy add src 10.244.3.10/24 dst 10.244.2.10/24 dir in  tmpl src 10.1.9.10 dst 10.1.8.10 proto esp reqid 0xfe51d979 mode tunnel
-
-      - iptables -t nat -A POSTROUTING -s 10.1.0.0/16 -o eth0 -j MASQUERADE
-
-      binds:
-        - ./ipsecdump.sh:/ipsecdump.sh
-
-    ipsec3:
-      kind: linux
-      image: 192.168.2.100:5000/nettool
-      exec:
-      - ip a a 10.244.3.1/24 dev eth1
-      - ip addr add 10.1.9.10/24 dev eth2
-      - ip r a 10.1.5.0/24 via 10.1.9.1 dev eth2
-      - ip r a 10.1.8.0/24 via 10.1.9.1 dev eth2
-
-      - ip xfrm state add src 10.1.9.10 dst 10.1.5.10 proto esp spi 0xfe51d978 reqid 0xfe51d978 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
-      - ip xfrm state add src 10.1.5.10 dst 10.1.9.10 proto esp spi 0xfe51d978 reqid 0xfe51d978 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
-
-      - ip xfrm state add src 10.1.9.10 dst 10.1.8.10 proto esp spi 0xfe51d979 reqid 0xfe51d979 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
-      - ip xfrm state add src 10.1.8.10 dst 10.1.9.10 proto esp spi 0xfe51d979 reqid 0xfe51d979 mode tunnel auth md5 0x00000000000000000000000000000002 enc aes 0x00000000000000000000000000000001
-
-
-      - ip xfrm policy add src 10.244.3.10/24 dst 10.244.1.10/24 dir out tmpl src 10.1.9.10 dst 10.1.5.10 proto esp reqid 0xfe51d978 mode tunnel
-      - ip xfrm policy add src 10.244.3.10/24 dst 10.244.1.10/24 dir fwd tmpl src 10.1.5.10 dst 10.1.9.10 proto esp reqid 0xfe51d978 mode tunnel
-      - ip xfrm policy add src 10.244.1.10/24 dst 10.244.3.10/24 dir in  tmpl src 10.1.5.10 dst 10.1.9.10 proto esp reqid 0xfe51d978 mode tunnel
-
-      - ip xfrm policy add src 10.244.3.10/24 dst 10.244.2.10/24 dir out tmpl src 10.1.9.10 dst 10.1.8.10 proto esp reqid 0xfe51d979 mode tunnel
-      - ip xfrm policy add src 10.244.3.10/24 dst 10.244.2.10/24 dir fwd tmpl src 10.1.8.10 dst 10.1.9.10 proto esp reqid 0xfe51d979 mode tunnel
-      - ip xfrm policy add src 10.244.2.10/24 dst 10.244.3.10/24 dir in  tmpl src 10.1.8.10 dst 10.1.9.10 proto esp reqid 0xfe51d979 mode tunnel
 
       - iptables -t nat -A POSTROUTING -s 10.1.0.0/16 -o eth0 -j MASQUERADE
 
@@ -112,20 +68,12 @@ topology:
       - ip addr add 10.244.2.10/24 dev net0
       - ip route replace default via 10.244.2.1
 
-    server3:
-      kind: linux
-      image: 192.168.2.100:5000/nettool
-      exec:
-      - ip addr add 10.244.3.10/24 dev net0
-      - ip route replace default via 10.244.3.1
 
   links:
     - endpoints: ["ipsec1:eth1", "server1:net0"]
     - endpoints: ["ipsec2:eth1", "server2:net0"]
-    - endpoints: ["ipsec3:eth1", "server3:net0"]
     - endpoints: ["ipsec1:eth2", "gwx:net1"]
     - endpoints: ["ipsec2:eth2", "gwx:net2"]
-    - endpoints: ["ipsec3:eth2", "gwx:net3"]
     
 EOF
 
