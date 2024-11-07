39f898658885188ea9d29179f983effee85e54bb
rowan <olaf.luo@foxmail.com>
2024-11-07 11:15:42 +0800

    Add rfc4543 udp mode[tunnel and transport]

--- LabasCode/netenv/4-adv-netwotk/11-IPSec/1-linux-ipsec/8-setup-ipsec-AES-GMAC-transport-mode-UDP.sh
+++ LabasCode/netenv/4-adv-netwotk/11-IPSec/1-linux-ipsec/8-setup-ipsec-AES-GMAC-transport-mode-UDP.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -v
+cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
 name: ipsec-transport-mode-gmac
 topology:
   nodes:
@@ -56,3 +59,4 @@ topology:
     - endpoints: ["ipsec1:eth2", "gwx:net1"]
     - endpoints: ["ipsec2:eth2", "gwx:net2"]
     
+EOF
