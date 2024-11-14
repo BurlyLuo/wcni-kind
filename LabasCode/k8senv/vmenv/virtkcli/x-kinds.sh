68e2e54170430726b4532ba9b528fa75f47c42a5
rowan <olaf.luo@foxmail.com>
2024-11-14 10:45:26 +0800

    Mod 3 to 3.

--- LabasCode/k8senv/vmenv/virtkcli/x-kinds.sh
+++ LabasCode/k8senv/vmenv/virtkcli/x-kinds.sh
@@ -40,7 +40,7 @@ ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.2.96"
 ssh 192.168.2.96 "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config.d/*.conf ; echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config && echo 'root:hive' | sudo chpasswd && systemctl restart ssh"
 sshpass -p hive ssh-copy-id -o StrictHostKeyChecking=no -p 22 root@192.168.2.96 > /dev/null 2>&1
 
-# 3 ubuntu2404
+# 3. ubuntu2404
 kcli create vm -i    ubuntu2404 -P memory=4096 -P disks=[50] -P rootpassword=hive -P nets="[{'name':'brnet','ip':'192.168.2.96','netmask':'24','gateway':'192.168.2.1'},{'name':'vppdpdk5'},{'name':'vppdpdk8'},{'name':'vppdpdk9'}]" -P cpupinning=['{"vcpus": "0", "hostcpus": "0"}','{"vcpus": "1", "hostcpus": "1"}','{"vcpus": "2", "hostcpus": "2"}','{"vcpus": 3, "hostcpus": 3}'] -P numcpus=4 -P cmds='[apt -y install net-tools pciutils wget lrzsz ; wget http://192.168.2.100/kvm/tools/lseth -P /usr/bin/ && chmod +x /usr/bin/lseth ; echo "TZ=Asia/Shanghai;export TZ" >> /etc/profile]' vm
 
 # 4. ubuntu2410
