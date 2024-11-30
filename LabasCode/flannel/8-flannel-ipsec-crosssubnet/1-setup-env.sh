16ad168457936b67444d53f57afd5711c23b4ad9
rowan <olaf.luo@foxmail.com>
2024-11-30 20:01:16 +0800

    Fix: flannel ipsec corsssubnet mode based on ip x s and ip x p

--- LabasCode/flannel/8-flannel-ipsec-crosssubnet/1-setup-env.sh
+++ LabasCode/flannel/8-flannel-ipsec-crosssubnet/1-setup-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -v
 
-# 1.prep noCNI env
+# 1. Prepare NoCNI environment
 cat <<EOF | kind create cluster --name=flannel-ipsec-crosssubnet --image=kindest/node:v1.27.3 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
@@ -51,16 +51,28 @@ containerdConfigPatches:
     endpoint = ["http://192.168.2.100:5000"]
 EOF
 
-# 2.remove taints
+# 2. Remove taints
 controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
 kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl get nodes -o wide
 
 ./2-setup-clab.sh
 
-# 3. install cni
+# 3. Collect startup message
+controller_node_name=$(kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep control-plane)
+if [ -n "$controller_node_name" ]; then
+  timeout 1 docker exec -t $controller_node_name bash -c 'cat << EOF > /root/monitor_startup.sh
+#!/bin/bash
+ip -ts monitor all > /root/startup_monitor.txt 2>&1
+EOF
+chmod +x /root/monitor_startup.sh && /root/monitor_startup.sh'
+else
+  echo "No such controller_node!"
+fi
+
+# 4. Install CNI(flannel ipsec tunnel mode) [https://github.com/flannel-io/flannel#deploying-flannel-with-kubectl]
 kubectl apply -f ./flannel.yaml
 
-# 4. wait all pods ready
+# 5. Wait all pods ready
 kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
 
