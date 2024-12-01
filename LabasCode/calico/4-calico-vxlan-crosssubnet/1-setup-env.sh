8ac74aa244d445a9ed1c5dbfe18dae095433c8a2
rowan <olaf.luo@foxmail.com>
2024-12-01 21:08:44 +0800

    Fix: fix calico vxlan crosssubenet with monitor_startup.sh script

--- LabasCode/calico/4-calico-vxlan-crosssubnet/1-setup-env.sh
+++ LabasCode/calico/4-calico-vxlan-crosssubnet/1-setup-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -v
 
-# 1.prep noCNI env
+# 1. Prepare NoCNI environment
 cat <<EOF | kind create cluster --name=calico-vxlan-crosssubnet --image=kindest/node:v1.27.3 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
@@ -51,19 +51,29 @@ containerdConfigPatches:
     endpoint = ["http://192.168.2.100:5000"]
 EOF
 
-# 2.remove taints
+# 2. Remove taints
 controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
 kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl get nodes -o wide
 
 ./2-setup-clab.sh
 
-# 3. install cni[Calico v3.23.2]
-kubectl apply -f ./calico.yaml
+# 3. Collect startup message
+controller_node_name=$(kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep control-plane)
+if [ -n "$controller_node_name" ]; then
+  timeout 1 docker exec -t $controller_node_name bash -c 'cat << EOF > /root/monitor_startup.sh
+#!/bin/bash
+ip -ts monitor all > /root/startup_monitor.txt 2>&1
+EOF
+chmod +x /root/monitor_startup.sh && /root/monitor_startup.sh'
+else
+  echo "No such controller_node!"
+fi
 
-# 4. wait all pods ready
+
+# 4. Install CNI[Calico v3.23.2]
+kubectl apply -f calico.yaml
+
+# 5. Wait all pods ready
 kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
-
-# 5. issue list
-# [xxx] https://xx.com
 
