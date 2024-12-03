9874a3f10426b928d16951c327f30cebb78a818b
rowan <olaf.luo@foxmail.com>
2024-12-03 13:25:21 +0800

    Add: add calico eBPF mode which running at multipass ubuntu2204 vm

--- LabasCode/calico/7-calico-eBPF/1-setup-env.sh
+++ LabasCode/calico/7-calico-eBPF/1-setup-env.sh
@@ -1,51 +1,49 @@
-#!/bin/bash
+#/bin/bash
 set -v
 
-# 1.prep noCNI env
-cat <<EOF | kind create cluster --name=calico-ebpf --image=kindest/node:v1.27.3 --config=-
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-networking:
-        disableDefaultCNI: true
-        kubeProxyMode: "none"
-nodes:
-        - role: control-plane
-        - role: worker
-        - role: worker
-
-containerdConfigPatches:
-- |-
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."192.168.2.100:5000"]
-    endpoint = ["http://192.168.2.100:5000"]
+# 1. Deploy multipass vmk(kubeProxyReplacement=true)
+for ((i=0; i<${1:-3}; i++))
+do
+  multipass launch 22.04 -n vmk"$i" -c 3 -m 3G -d 30G --cloud-init - <<EOF
+  # cloud-config
+  runcmd:
+    - sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+    - sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config.d/*.conf
+    - sudo echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config && echo 'root:hive' | sudo chpasswd && systemctl restart sshd
+    - sudo mkdir -p /etc/rancher/k3s/ && wget http://192.168.2.100/k3s/registries.yaml -P /etc/rancher/k3s/
+    - sudo wget -r -np -nH --cut-dirs=3 --directory-prefix=/opt/cni/bin/ http://192.168.2.100/k3s/cni/bin/ && find /opt/cni/bin/ -type f | xargs chmod +x
+    - sudo bash -c '{ echo "alias all=\"kubectl get pods -A\""; echo "alias k=\"kubectl\""; echo "alias kk=\"kubectl -nkube-system\"" ; } >> ~/.bashrc'
 EOF
+done
 
-# 2.remove taints
-controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
-kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/control-plane:NoSchedule-
-kubectl get nodes -o wide
+# 2. prep env[ubuntu 22.04]
+mapfile -t ip_addresses < <(multipass list | grep -E 'vmk' | awk '{print $3}')
 
-# 3. install CNI[Calico v3.23.2]
+for ((ip_id=0; ip_id<${#ip_addresses[@]}; ip_id++)); do
+    sshpass -p hive ssh-copy-id -o StrictHostKeyChecking=no -p 22 root@${ip_addresses[$ip_id]} > /dev/null 2>&1
+
+    echo "${ip_addresses[$ip_id]} vmk$ip_id" >> /etc/hosts
+
+    master_ip=${ip_addresses[0]}
+    k3s_version="v1.27.3+k3s1"
+
+    if [ $ip_id -eq 0 ]; then
+        # --k3s-extra-args can refer: k3s server -h | grep flannel-backend
+        k3sup install --ip=$master_ip --user=root --merge --sudo --cluster --k3s-version=v1.27.3+k3s1 --k3s-extra-args "--flannel-backend=none --cluster-cidr=10.244.0.0/16 --disable-network-policy --disable-kube-proxy --disable traefik --disable servicelb --node-ip=$master_ip" --local-path $HOME/.kube/config --context=kpr
+    else
+        k3sup join --ip ${ip_addresses[$ip_id]} --user root --sudo --k3s-version=$k3s_version --server-ip $master_ip --server-user root
+    fi
+done
+
+# 3. replace calico-bpf.yaml's KUBERNETES_SERVICE_HOST
 KUBERNETES_SERVICE_HOST=`kubectl get nodes --selector=node-role.kubernetes.io/control-plane -o jsonpath='{$.items[*].status.addresses[?(@.type=="InternalIP")].address}'`
-echo $KUBERNETES_SERVICE_HOST
-cat <<EOF | kubectl apply -f -
-kind: ConfigMap
-apiVersion: v1
-metadata:
-  name: kubernetes-services-endpoint
-  namespace: kube-system
-data:
-  KUBERNETES_SERVICE_HOST: "${KUBERNETES_SERVICE_HOST}"
-  KUBERNETES_SERVICE_PORT: "6443"
+sed -i "s/kubernetes_service_host: \"\"/kubernetes_service_host: \"${KUBERNETES_SERVICE_HOST}\"/g" calico-bpf.yaml
 
-EOF
-
-
-
-# 4. Enable BPF
-kubectl -nkube-system wait --timeout=60s --for=condition=Ready=true pod -l k8s-app=calico-node
+kubectl apply -f ./calico-bpf.yaml
 
 calicoctl --allow-version-mismatch patch felixconfiguration default --patch='{"spec": {"bpfEnabled": true}}'
 
-# 5. wait all pods ready
-kubectl wait --timeout=150s --for=condition=Ready=true pods --all -A
+kubectl -nkube-system rollout restart ds/calico-node
+
+kubectl -nkube-system wait --timeout=160s --for=condition=Ready=true pod -l k8s-app=calico-node
 
