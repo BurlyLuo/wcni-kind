cbe49d5e1d72b04d916cc5b3a389375c5812e77b
rowan <olaf.luo@foxmail.com>
2024-12-04 10:28:44 +0800

    Fix: unify the multus with flannel cni to provide muti-interface demo

--- LabasCode/multus/40-kind-multus-flannel-macvlan/1-setup-env.sh
+++ LabasCode/multus/40-kind-multus-flannel-macvlan/1-setup-env.sh
@@ -2,16 +2,16 @@
 date
 set -v
 
-# 1.prep noCNI env
+# 1. prep noCNI env
 cat <<EOF | kind create cluster --name=cni-multus --image=kindest/node:v1.27.3 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
-        disableDefaultCNI: true
+  disableDefaultCNI: true
 nodes:
-        - role: control-plane
-        - role: worker
-        - role: worker
+  - role: control-plane
+  - role: worker
+  - role: worker
 
 containerdConfigPatches:
 - |-
@@ -19,7 +19,7 @@ containerdConfigPatches:
     endpoint = ["http://192.168.2.100:5000"]
 EOF
 
-# 2.remove taints
+# 2. remove taints
 controller_node_ip=`kubectl get node -o wide --no-headers | grep -E "control-plane|bpf1" | awk -F " " '{print $6}'`
 kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl get nodes -o wide
