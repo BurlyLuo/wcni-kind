b8c87f475326bc0eb1625bf09cfe5ee3624713ff
rowan <olaf.luo@foxmail.com>
2024-12-16 19:26:36 +0800

    vpp-calico: Add details for setup a multi-node calico-vpp based environment for reference

--- LabasCode/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/29-vpp-calico/1-setup-env.sh
+++ LabasCode/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/29-vpp-calico/1-setup-env.sh
@@ -2,3 +2,16 @@ kcli create vm -i k3s_compressed -P numcpus=4 -P memory=4096 -P disks=[50] -P ro
 
 kcli create vm -i k3s_compressed -P numcpus=4 -P memory=4096 -P disks=[50] -P rootpassword=hive -P nets="[{'name':'brnet','ip':'192.168.2.52','netmask':'24','gateway':'192.168.2.1'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk8'},{'name':'vppdpdk8'},{'name':'vppdpdk9'},{'name':'vppdpdk9'}]" k3s2
 
+# addk3svm to setup a multi-node k3s cluster.
+addk3svm
+
+# delk3svm to destroy a multi-node k3s cluster.
+delk3svm
+
+1. how to install the vpp based calico
+   https://github.com/projectcalico/vpp-dataplane/issues/222
+1. https://docs.tigera.io/calico/latest/operations/troubleshoot/troubleshooting#configure-networkmanager
+2. kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.1/manifests/calico.yaml
+3. curl -o calico-vpp.yaml https://raw.githubusercontent.com/projectcalico/vpp-dataplane/v3.29.0/yaml/generated/calico-vpp-nohuge.yaml  # replace the "interfaceName": "eth0" with default route interface name
+4. kubectl create -f calico-vpp.yaml
+
