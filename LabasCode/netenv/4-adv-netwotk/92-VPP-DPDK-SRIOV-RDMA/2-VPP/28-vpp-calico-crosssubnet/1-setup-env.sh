ab3c09731787a80cf3017af40ce310bafc03e1da
rowan <olaf.luo@foxmail.com>
2024-12-18 17:22:07 +0800

    calico-vpp: gc the tmp for calico-vpp mode

--- LabasCode/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/28-vpp-calico-crosssubnet/1-setup-env.sh
+++
@@ -1,4 +0,0 @@
-kcli create vm -i k3s_compressed -P numcpus=4 -P memory=4096 -P disks=[50] -P rootpassword=hive -P nets="[{'name':'vppdpdk5','ip':'10.1.5.51','netmask':'24','gateway':'10.1.5.3'}]" k1
-kcli create vm -i k3s_compressed -P numcpus=4 -P memory=4096 -P disks=[50] -P rootpassword=hive -P nets="[{'name':'vppdpdk8','ip':'10.1.8.52','netmask':'24','gateway':'10.1.8.3'}]" k2
-
-
