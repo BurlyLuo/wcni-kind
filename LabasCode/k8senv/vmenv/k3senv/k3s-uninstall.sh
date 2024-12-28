c30f23e0bcc920287b7c93b42eaf38914a78c11a
rowan <olaf.luo@foxmail.com>
2024-12-28 22:09:32 +0800

    bashrc: fix addk3svms and delk3svms to adapt offline install k3s cluster with k3sup tool

--- LabasCode/k8senv/vmenv/k3senv/k3s-uninstall.sh
+++ LabasCode/k8senv/vmenv/k3senv/k3s-uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+set -v
 [ $(id -u) -eq 0 ] || exec sudo $0 $@
 
 /usr/local/bin/k3s-killall.sh
@@ -16,14 +16,13 @@ fi
 rm -f /etc/systemd/system/k3s.service
 rm -f /etc/systemd/system/k3s.service.env
 
-remove_uninstall() {
-    rm -f /usr/local/bin/k3s-uninstall.sh
-}
-trap remove_uninstall EXIT
+#remove_uninstall() {
+#    rm -f /usr/local/bin/k3s-uninstall.sh
+#}
+#trap remove_uninstall EXIT
 
 if (ls /etc/systemd/system/k3s*.service || ls /etc/init.d/k3s*) >/dev/null 2>&1; then
     set +x; echo 'Additional k3s services installed, skipping uninstall of k3s'; set -x
-    exit
 fi
 
 for cmd in kubectl crictl ctr; do
@@ -37,20 +36,24 @@ rm -rf /run/k3s
 rm -rf /run/flannel
 rm -rf /var/lib/rancher/k3s
 rm -rf /var/lib/kubelet
-rm -f /usr/local/bin/k3s
-rm -f /usr/local/bin/k3s-killall.sh
+#rm -f /usr/local/bin/k3s
+#rm -f /usr/local/bin/k3s-killall.sh
 
-if type yum >/dev/null 2>&1; then
-    yum remove -y k3s-selinux
-    rm -f /etc/yum.repos.d/rancher-k3s-common*.repo
-elif type rpm-ostree >/dev/null 2>&1; then
-    rpm-ostree uninstall k3s-selinux
-    rm -f /etc/yum.repos.d/rancher-k3s-common*.repo
-elif type zypper >/dev/null 2>&1; then
-    uninstall_cmd="zypper remove -y k3s-selinux"
-    if [ "${TRANSACTIONAL_UPDATE=false}" != "true" ] && [ -x /usr/sbin/transactional-update ]; then
-        uninstall_cmd="transactional-update --no-selfupdate -d run $uninstall_cmd"
-    fi
-    $uninstall_cmd
-    rm -f /etc/zypp/repos.d/rancher-k3s-common*.repo
-fi
+#if type yum >/dev/null 2>&1; then
+#    yum remove -y k3s-selinux
+#    rm -f /etc/yum.repos.d/rancher-k3s-common*.repo
+#elif type rpm-ostree >/dev/null 2>&1; then
+#    rpm-ostree uninstall k3s-selinux
+#    rm -f /etc/yum.repos.d/rancher-k3s-common*.repo
+#elif type zypper >/dev/null 2>&1; then
+#    uninstall_cmd="zypper remove -y k3s-selinux"
+#    if [ "${TRANSACTIONAL_UPDATE=false}" != "true" ] && [ -x /usr/sbin/transactional-update ]; then
+#        uninstall_cmd="transactional-update --no-selfupdate -d run $uninstall_cmd"
+#    fi
+#    $uninstall_cmd
+#    rm -f /etc/zypp/repos.d/rancher-k3s-common*.repo
+#fi
+
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
