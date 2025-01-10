1b958911fe796a438b7a6f39d8d7e6e75b659681
rowan <olaf.luo@foxmail.com>
2025-01-10 20:11:44 +0800

    vpp: add the details about vpp qdisc-bypass-enable to understand why the tcpdump can't capture the pkt(and such packets are also not visible to other PF_PACKET sockets anymore)

--- LabasCode/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/21-vpp-af-packet/2-vpp-macvlan/1-setup-env.sh
+++ LabasCode/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/21-vpp-af-packet/2-vpp-macvlan/1-setup-env.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# vpp> show hard> qdisc-bypass-enable
+# If there is a requirement to load the network with many packets in a similar
+# fashion as pktgen does, you might set the following option after socket
+# creation:
+
+#    int one = 1;
+#    setsockopt(fd, SOL_PACKET, PACKET_QDISC_BYPASS, &one, sizeof(one));
+
+# This has the side-effect, that packets sent through PF_PACKET will bypass the
+# kernel's qdisc layer and are forcedly pushed to the driver directly. Meaning,
+# packet are not buffered, tc disciplines are ignored, increased loss can occur
+# and such packets are also not visible to other PF_PACKET sockets anymore. So,
+# you have been warned; generally, this can be useful for stress testing various
+# components of a system.
+
 set -v
 cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
 name: cnf
