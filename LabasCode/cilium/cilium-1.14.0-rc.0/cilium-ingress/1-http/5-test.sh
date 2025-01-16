3cf97539f36ccea6a458ee04a6ceec98b3838349
rowan <olaf.luo@foxmail.com>
2025-01-16 17:32:29 +0800

    cilium: test cilium ingress func with hostRouting mode[--set bpf.masquerade=true]

--- LabasCode/cilium/cilium-1.14.0-rc.0/cilium-ingress/1-http/5-test.sh
+++ LabasCode/cilium/cilium-1.14.0-rc.0/cilium-ingress/1-http/5-test.sh
@@ -3,6 +3,6 @@ set -v
 
 # 1.Cilium ingress http demo
 HTTP_INGRESS=$(kubectl get ingress basic-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-curl -v --fail -s http://"$HTTP_INGRESS"/details/1 | jq
+curl -v --fail -s http://"$HTTP_INGRESS"/details/1 | jq 
 
 
