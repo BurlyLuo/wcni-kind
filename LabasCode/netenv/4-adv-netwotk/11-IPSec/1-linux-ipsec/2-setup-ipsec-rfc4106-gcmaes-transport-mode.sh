1219e3a356a25bd0308400b35a2ecb8294c62c1b
rowan <olaf.luo@foxmail.com>
2025-03-08 08:38:36 +0800

    ipsec: add rfc4106 details about the key(salt value) length and icv length

--- LabasCode/netenv/4-adv-netwotk/11-IPSec/1-linux-ipsec/2-setup-ipsec-rfc4106-gcmaes-transport-mode.sh
+++ LabasCode/netenv/4-adv-netwotk/11-IPSec/1-linux-ipsec/2-setup-ipsec-rfc4106-gcmaes-transport-mode.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 set -v
+# https://www.rfc-editor.org/rfc/rfc4106.txt
+cat <<EOF
+8.0.  Key Length Attribute
+
+   Because the AES supports three key lengths, the Key Length attribute
+   MUST be specified in the IKE Phase 2 exchange [RFC2407].  The Key
+   Length attribute MUST have a value of 128, 192, or 256.
+
+8.1.  Keying Material and Salt Values
+
+   IKE makes use of a pseudo-random function (PRF) to derive keying
+   material.  The PRF is used iteratively to derive keying material of
+   arbitrary size, called KEYMAT.  Keying material is extracted from the
+   output string without regard to boundaries.
+
+   The size of the KEYMAT for the AES-GCM-ESP MUST be four octets longer
+   than is needed for the associated AES key.  The keying material is
+   used as follows:
+
+   AES-GCM-ESP with a 128 bit key
+      The KEYMAT requested for each AES-GCM key is 20 octets.  The first
+      16 octets are the 128-bit AES key, and the remaining four octets
+      are used as the salt value in the nonce.
+
+   AES-GCM-ESP with a 192 bit key
+      The KEYMAT requested for each AES-GCM key is 28 octets.  The first
+      24 octets are the 192-bit AES key, and the remaining four octets
+      are used as the salt value in the nonce.
+
+   AES-GCM-ESP with a 256 bit key
+      The KEYMAT requested for each AES GCM key is 36 octets.  The first
+      32 octets are the 256-bit AES key, and the remaining four octets
+      are used as the salt value in the nonce.
+
+8.3.  Phase 2 Identifier
+
+   For IKE Phase 2 negotiations, IANA has assigned three ESP Transform
+   Identifiers for AES-GCM with an eight-byte explicit IV:
+
+      18 for AES-GCM with an 8 octet ICV;
+      19 for AES-GCM with a 12 octet ICV; and
+      20 for AES-GCM with a 16 octet ICV.
+
+EOF
+
 cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
 name: ipsec-tunnel-mode-aes-gcm
 topology:
