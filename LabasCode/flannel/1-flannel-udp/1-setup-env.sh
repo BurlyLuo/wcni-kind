6f394f03f36dffac9cbdba033a76520ebe05b683
rowan <olaf.luo@foxmail.com>
2025-04-01 17:45:55 +0800

    flannel: add verbose install log for kind k8s

--- LabasCode/flannel/1-flannel-udp/1-setup-env.sh
+++ LabasCode/flannel/1-flannel-udp/1-setup-env.sh
@@ -167,7 +167,7 @@ else
   echo "INFO: No such k8s cluster $k8s_name. Creating..."
 fi
 
-kind get clusters | grep -wq $k8s_name || cat <<EOF | KIND_EXPERIMENTAL_DOCKER_NETWORK=kind kind create cluster --name=$k8s_name --image=$image_name --config=-
+kind get clusters | grep -wq $k8s_name || cat <<EOF | KIND_EXPERIMENTAL_DOCKER_NETWORK=kind kind create cluster --name=$k8s_name --image=$image_name -v=9 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
