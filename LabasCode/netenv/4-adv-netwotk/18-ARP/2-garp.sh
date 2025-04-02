2c6b2dbf73f6c10be733bc0b1b6051f9ebe8cae4
rowan <olaf.luo@foxmail.com>
2025-04-02 17:56:55 +0800

    arp: add garp to send arp

---
+++ LabasCode/netenv/4-adv-netwotk/18-ARP/2-garp.sh
@@ -0,0 +1,8 @@
+arping -U -c 1 -I brnet 192.168.2.99
+
+# pcap:
+160123	2025-04-02 17:53:31.403027514			ARP	58							Gratuitous ARP for 192.168.2.99 (Request)
+Frame 160123: 58 bytes on wire (464 bits), 58 bytes captured (464 bits) on interface ens33, id 0
+Ethernet II, Src: 86:f7:f9:f7:75:94 (86:f7:f9:f7:75:94), Dst: Broadcast (ff:ff:ff:ff:ff:ff)
+Address Resolution Protocol (request/gratuitous ARP)
+
