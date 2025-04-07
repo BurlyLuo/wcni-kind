b711d94cf9a0134f9594bb4b01fdc8e302317c76
rowan <olaf.luo@foxmail.com>
2025-04-07 13:24:34 +0800

    mtu: fix the use-case for the jumbo frame

--- LabasCode/netenv/4-adv-netwotk/35-MTU/5-Jumbo-Frame/1-setup-clab.sh
+++ LabasCode/netenv/4-adv-netwotk/35-MTU/5-Jumbo-Frame/1-setup-clab.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -v
+{ ip l s br-vlan down; ip l d br-vlan; } > /dev/null 2>&1
+ip l a br-vlan type bridge
+ip l s br-vlan up
+
 cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
 name: mtu-9000
 topology:
   nodes:
     gw1:
       kind: linux
-      image: 192.168.2.100:5000/nettool
+      image: 192.168.2.100:5000/xcni
       exec:
       - ip a a 10.1.5.1/24 dev eth1
       - ip a a 172.18.0.2/24 dev eth2
@@ -15,32 +19,45 @@ topology:
 
     gw2:
       kind: linux
-      image: 192.168.2.100:5000/nettool
+      image: 192.168.2.100:5000/xcni
       exec:
       - ip a a 10.1.8.1/24 dev eth1
       - ip a a 172.18.0.3/24 dev eth2
       - ip r a 10.1.5.0/24 dev eth2 via 172.18.0.2 
       - iptables -t nat -A POSTROUTING -s 10.1.0.0/16 -o eth0 -j MASQUERADE
 
+    br-vlan:
+      kind: bridge 
+
     server1:
       kind: linux
-      image: 192.168.2.100:5000/nettool
+      image: 192.168.2.100:5000/xcni
       exec:
       - ip addr add 10.1.5.10/24 dev net0
       - ip route replace default via 10.1.5.1
-
     server2:
       kind: linux
-      image: 192.168.2.100:5000/nettool
+      image: 192.168.2.100:5000/xcni
+      exec:
+      - ip addr add 10.1.5.11/24 dev net0
+      - ip route replace default via 10.1.5.1
+
+    server3:
+      kind: linux
+      image: 192.168.2.100:5000/xcni
       exec:
       - ip addr add 10.1.8.10/24 dev net0
       - ip route replace default via 10.1.8.1
 
   links:
-    - endpoints: ["gw1:eth1", "server1:net0"]
+    - endpoints: ["br-vlan:eth1", "server1:net0"]
       mtu: 9000
-    - endpoints: ["gw2:eth1", "server2:net0"]
+    - endpoints: ["br-vlan:eth2", "server2:net0"]
       mtu: 9000
+    - endpoints: ["br-vlan:eth3", "gw1:eth1"]
+      mtu: 1500
+    - endpoints: ["gw2:eth1", "server3:net0"]
+      mtu: 1500
     - endpoints: ["gw1:eth2", "gw2:eth2"]
-      mtu: 9000
+      mtu: 1500
 EOF
