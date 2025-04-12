85529f3f8a73d0de4fa5890ac3f266c007e070ee
rowan <olaf.luo@foxmail.com>
2025-04-12 18:44:38 +0800

    vpp: add vpp lcp mode

--- LabasCode/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/02-vpp-lcpng/1-vpp-lcp/1-setup-env.sh
+++ LabasCode/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/02-vpp-lcpng/1-vpp-lcp/1-setup-env.sh
@@ -1,42 +1,7 @@
-kcli create vm -i  vpp-proto-bookworm -P numcpus=4 -P memory=4096 -P disks=[50] -P nets="[{'name':'brnet'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk8'},{'name':'vppdpdk8'},{'name':'vppdpdk9'},{'name':'vppdpdk9'}]" lcp1
+#!/bin/bash
+set -v
 
-cat <<EOF>/etc/netplan/01-netcfg.yaml
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    ens3:
-      addresses: [192.168.2.71/24]
-      gateway4: 192.168.2.1
-      nameservers: 
-        addresses: [192.168.2.1]
-      optional: true
-      accept-ra: true
-      dhcp4: false
-      dhcp6: true
-EOF
-netplan apply
-
-scp -r ./71vm/* 192.168.2.71:/etc/vpp/ && ssh 192.168.2.71 "systemctl restart vpp"
-
-kcli create vm -i  vpp-proto-bookworm -P numcpus=4 -P memory=4096 -P disks=[50] -P nets="[{'name':'brnet'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk8'},{'name':'vppdpdk8'},{'name':'vppdpdk9'},{'name':'vppdpdk9'}]" lcp2
-
-cat <<EOF>/etc/netplan/01-netcfg.yaml
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    ens3:
-      addresses: [192.168.2.72/24]
-      gateway4: 192.168.2.1
-      nameservers:
-        addresses: [192.168.2.1]
-      optional: true
-      accept-ra: true
-      dhcp4: false
-      dhcp6: true
-EOF
-netplan apply
-
-scp -r ./72vm/* 192.168.2.72:/etc/vpp/ && ssh 192.168.2.72 "systemctl restart vpp"
-
+for ip in 192.168.2.71 192.168.2.72; do
+    kcli create vm -i ipng -P numcpus=4 -P memory=4096 -P disks=[50] -P nets="[{'name':'brnet','ip':'${ip}','netmask':'24','gateway':'192.168.2.1'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk8'},{'name':'vppdpdk8'},{'name':'vppdpdk9'},{'name':'vppdpdk9'}]" $ip
+    sleep 1
+done
