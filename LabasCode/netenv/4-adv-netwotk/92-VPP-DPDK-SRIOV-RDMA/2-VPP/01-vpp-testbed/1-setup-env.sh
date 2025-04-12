787685eda2ebd9a99bd701b851580906efcdb985
rowan <olaf.luo@foxmail.com>
2025-04-12 18:44:38 +0800

    vpp: add vpp lcp mode

---
+++ LabasCode/netenv/4-adv-netwotk/92-VPP-DPDK-SRIOV-RDMA/2-VPP/01-vpp-testbed/1-setup-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -v
+
+for ip in 192.168.2.71 192.168.2.72; do
+    id=$(echo $ip | awk -F "." '{print $NF}')
+    kcli create vm -i ipng$id -P numcpus=4 -P memory=4096 -P disks=[50] -P nets="[{'name':'brnet','ip':'$ip','netmask':'24','gateway':'192.168.2.1'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk5'},{'name':'vppdpdk8'},{'name':'vppdpdk8'},{'name':'vppdpdk9'},{'name':'vppdpdk9'}]" $ip
+done
