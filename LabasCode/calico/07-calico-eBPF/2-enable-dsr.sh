e11c73e335a913a871b3ebaf0465da5a0780b735
rowan <olaf.luo@foxmail.com>
2025-04-15 15:14:29 +0800

    calico: add calico DSR topo for reference

--- LabasCode/calico/07-calico-eBPF/2-enable-dsr.sh
+++ LabasCode/calico/07-calico-eBPF/2-enable-dsr.sh
@@ -2,5 +2,13 @@
 set -v 
 
 # 1. Enable DSR
-calicoctl --allow-version-mismatch patch felixconfiguration default --patch='{"spec": {"bpfExternalServiceMode": "DSR"}}'
+calicoctl patch felixconfiguration default --patch='{"spec": {"bpfExternalServiceMode": "DSR"}}'
 
+# DST TOPO 
+cat <<EOF
+                  SYN                       SYN[VxLAN]
+Local_IP[11.1.1.1]--->LB1_NODE:32000[11.1.1.2]--->LB2_NODE(BackendNode)[11.1.1.3]-|
+         |                                                                        |
+         |------------------------------------------------------------------------|                  
+                                         SYN+ACK
+EOF
