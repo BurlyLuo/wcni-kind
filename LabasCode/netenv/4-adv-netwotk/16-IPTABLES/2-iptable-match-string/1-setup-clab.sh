4a4d51793efe53bdebffaec262399cc5dff6c846
rowan <olaf.luo@foxmail.com>
2025-04-19 07:18:30 +0800

    iptable: add iptables with string mode to match spec values

---
+++ LabasCode/netenv/4-adv-netwotk/16-IPTABLES/2-iptable-match-string/1-setup-clab.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -v
+cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
+name: tcp-match-string
+mgmt:
+  ipv6-subnet: ""
+  ipv4-subnet: 172.20.20.0/24
+
+topology:
+  nodes:
+    gw1:
+      kind: linux
+      image: 192.168.2.100:5000/xcni
+      exec:
+      - ip a a 10.1.5.1/24 dev eth1
+      - ip a a 10.1.8.1/24 dev eth2
+      # block string: Host: 10.1.8.10\r\n
+      - iptables -A FORWARD -m string --hex-string "|486f73743a2031302e382e31300d0a|" --algo bm --to 65535 -j DROP
+
+    client:
+      kind: linux
+      image: 192.168.2.100:5000/xcni
+      exec:
+      - ip addr add 10.1.5.10/24 dev net0
+      - ip r r default via 10.1.5.1 dev net0
+
+    server:
+      kind: linux
+      image: 192.168.2.100:5000/xcni
+      exec:
+      - ip addr add 10.1.8.10/24 dev net0
+      - ip r r default via 10.1.8.1 dev net0
+
+  links:
+    - endpoints: ["gw1:eth1", "client:net0"]
+    - endpoints: ["gw1:eth2", "server:net0"]
+
+EOF
