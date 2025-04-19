49171313b70efd37762079daadd6e5134bae3b3f
rowan <olaf.luo@foxmail.com>
2025-04-19 12:44:05 +0800

    chore(project): refactor directory/file naming convention

--- LabasCode/netenv/4-adv-netwotk/36-TCP-3Ways-handshake/5-tcp-server-SYN_RECV/1-setup-clab.sh
+++ LabasCode/netenv/4-adv-netwotk/36-TCP-3Ways-handshake/5-tcp-server-SYN_RECV/1-setup-clab.sh
@@ -2,10 +2,6 @@
 set -v
 cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
 name: tcp-syn-recv
-mgmt:
-  ipv6-subnet: ""
-  ipv4-subnet: 172.20.20.0/24
-
 topology:
   nodes:
     gw1:
@@ -17,14 +13,14 @@ topology:
       - iptables -A FORWARD -s 10.1.8.10 -d 10.1.5.10 -p tcp --tcp-flags SYN,ACK SYN,ACK -j DROP
       - iptables -t nat -A POSTROUTING -s 10.1.0.0/16 -o eth0 -j MASQUERADE
 
-    server1:
+    client:
       kind: linux
       image: 192.168.2.100:5000/xcni
       exec:
       - ip addr add 10.1.5.10/24 dev net0
       - ip r r default via 10.1.5.1 dev net0
 
-    server2:
+    server:
       kind: linux
       image: 192.168.2.100:5000/xcni
       exec:
@@ -32,19 +28,18 @@ topology:
       - ip r r default via 10.1.8.1 dev net0
 
   links:
-    - endpoints: ["gw1:eth1", "server1:net0"]
-    - endpoints: ["gw1:eth2", "server2:net0"]
-
+    - endpoints: ["gw1:eth1", "client:net0"]
+    - endpoints: ["gw1:eth2", "server:net0"]
 EOF
 
 # 1. cmd:
 # iptables -A FORWARD -s 10.1.8.10 -d 10.1.5.10 -p tcp --tcp-flags SYN,ACK SYN,ACK -j DROP
 
 # 2. curl the dst ip:port
-# [root@server1 /]# curl 10.1.8.10 
+# [root@client /]# curl 10.1.8.10 
 # ...  waiting...
 
-# 3. monitor the netstat outputs[server2]
+# 3. monitor the netstat outputs[server]
 # while true;do netstat -apn | grep SYN;done
 # tcp        0      0 10.1.8.10:80            10.1.5.10:37552         SYN_RECV    -                   
 # tcp        0      0 10.1.8.10:80            10.1.5.10:37552         SYN_RECV    -                   
@@ -52,4 +47,3 @@ EOF
 # tcp        0      0 10.1.8.10:80            10.1.5.10:37552         SYN_RECV    -                   
 # tcp        0      0 10.1.8.10:80            10.1.5.10:37552         SYN_RECV    -                   
 # tcp        0      0 10.1.8.10:80            10.1.5.10:37552         SYN_RECV    -   
-
