8cb02ceb294d79b4b8102026db504ff351689cf4
rowan <olaf.luo@foxmail.com>
2025-05-09 10:37:56 +0800

    cca: Add cilium gateway api support

---
+++ LabasCode/netenv/4-adv-network/CCA/4-Service-Mesh/2-Gateway-API-Support/1-http/5-test.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+set -v
+
+# 1. http gateway-api test 
+GATEWAY=$(kubectl get gateway my-gateway -o jsonpath='{.status.addresses[0].value}')
+curl -v http://"$GATEWAY"/details/1 | jq
+
+curl -v "http://$GATEWAY/?great=example" -H "magic: foo" | jq
+
+# node_name=$(docker ps -a --format "table {{.Names}}" | grep cilium-gwapi-http | grep control-plane)
+# docker exec -it $node_name tcpdump -pne -c 2 -i eth0 host $GATEWAY & > /dev/null 2>&1 &
+
