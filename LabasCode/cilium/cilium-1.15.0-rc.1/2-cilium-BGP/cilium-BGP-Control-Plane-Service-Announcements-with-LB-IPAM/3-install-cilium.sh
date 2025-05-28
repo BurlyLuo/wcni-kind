77fdfbbe83fc6ec779a849ac5fa6838a8b17a035
rowan <olaf.luo@foxmail.com>
2025-05-28 20:31:52 +0800

    cilium: Fix cilium bgp solution with mtu issue

--- LabasCode/cilium/cilium-1.15.0-rc.1/2-cilium-BGP/cilium-BGP-Control-Plane-Service-Announcements-with-LB-IPAM/3-install-cilium.sh
+++ LabasCode/cilium/cilium-1.15.0-rc.1/2-cilium-BGP/cilium-BGP-Control-Plane-Service-Announcements-with-LB-IPAM/3-install-cilium.sh
@@ -4,8 +4,8 @@ set -v
 kubectl taint nodes $(kubectl get nodes -o name | grep control-plane) node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl get nodes -o wide
 
-# 2.Install Cilium[Cilium v1.15.17]
-cilium_version=v1.15.17
+# 2.Install Cilium[Cilium v1.15.7]
+cilium_version=v1.15.7
 docker pull quay.io/cilium/cilium:$cilium_version && docker pull quay.io/cilium/operator-generic:$cilium_version
 kind load docker-image quay.io/cilium/cilium:$cilium_version quay.io/cilium/operator-generic:$cilium_version --name cilium-bgp
 { helm repo add cilium https://helm.cilium.io ; helm repo update; } > /dev/null 2>&1
@@ -19,7 +19,7 @@ kind load docker-image quay.io/cilium/cilium:$cilium_version quay.io/cilium/oper
 controller_node_ip=`docker exec -it $(docker ps -a --format "table {{.Names}}" | grep control) ip a s eth0 | awk -F "inet " '{print $2}' | grep 172.18.0. | awk -F "/16" '{print $1}'`
 # --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443
 
-helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version v1.15.17 --namespace kube-system --set image.pullPolicy=IfNotPresent --set debug.enabled=true --set debug.verbose="datapath flow kvstore envoy policy" --set bpf.monitorAggregation=none --set monitor.enabled=true --set ipam.mode=kubernetes --set routingMode=native --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set bgpControlPlane.enabled=true --set k8s.requireIPv4PodCIDR=true
+helm install cilium cilium/cilium --set k8sServiceHost=$controller_node_ip --set k8sServicePort=6443 --version 1.15.7 --namespace kube-system --set image.pullPolicy=IfNotPresent --set debug.enabled=true --set debug.verbose="datapath flow kvstore envoy policy" --set bpf.monitorAggregation=none --set monitor.enabled=true --set ipam.mode=kubernetes --set routingMode=native --set ipv4NativeRoutingCIDR=10.0.0.0/8 --set bgpControlPlane.enabled=true --set k8s.requireIPv4PodCIDR=true
 
 # 3. Wait all pods ready
 kubectl wait --timeout=100s --for=condition=Ready=true pods --all -A
