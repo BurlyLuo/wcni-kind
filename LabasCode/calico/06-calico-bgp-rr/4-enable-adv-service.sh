39185b84e8845fe37d367427c959c19aa8b36e73
rowan <olaf.luo@foxmail.com>
2025-06-05 09:38:46 +0800

    calico: Add different service type with bgp advertise

--- LabasCode/calico/06-calico-bgp-rr/4-enable-adv-service.sh
+++ LabasCode/calico/06-calico-bgp-rr/4-enable-adv-service.sh
@@ -1,1 +1,11 @@
+# 1. Advertise serviceClusterIPs
 calicoctl --allow-version-mismatch patch BGPConfig default --patch '{"spec": {"serviceClusterIPs": [{"cidr": "10.96.0.0/16"}]}}'
+
+# 2. Advertise external IP address
+calicoctl --allow-version-mismatch patch bgpconfig default --patch '{"spec": {"serviceExternalIPs": [{"cidr": "20.0.0.0/16"}, {"cidr": "30.0.0.0/16"}]}}'
+
+# 3. Advertise service load balancer IP addresses
+calicoctl --allow-version-mismatch patch bgpconfig default --patch '{"spec": {"serviceLoadBalancerIPs": [{"cidr": "40.0.0.0/16"}]}}'
+
+# 4. List bgpconfig
+calicoctl --allow-version-mismatch get bgpconfig default -o yaml
