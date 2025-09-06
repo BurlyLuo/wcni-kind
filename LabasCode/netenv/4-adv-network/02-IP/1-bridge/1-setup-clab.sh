eb8c2d4ebe0148753dfe6744309710cd361e9fe1
rowan <olaf.luo@foxmail.com>
2025-09-06 17:35:10 +0800

    Fix ip6 testbed

--- LabasCode/netenv/4-adv-network/02-IP/1-bridge/1-setup-clab.sh
+++ LabasCode/netenv/4-adv-network/02-IP/1-bridge/1-setup-clab.sh
@@ -15,17 +15,33 @@ topology:
       kind: linux
       image: 192.168.2.100:5000/nettool
       exec:
-      - ip addr add 10.1.5.10/24 dev net0
+      - ip a a 10.1.5.10/24 dev net0
+      - ip -6 a a 10:1:5::10/64 dev net0
+      - ip l s dev net0 address aa:c1:ab:00:00:10
 
     server2:
       kind: linux
       image: 192.168.2.100:5000/nettool
       exec:
-      - ip addr add 10.1.5.11/24 dev net0
+      - ip a a 10.1.5.11/24 dev net0
+      - ip -6 a a 10:1:5::11/64 dev net0
+      - ip l s dev net0 address aa:c1:ab:00:00:11
+
+
+    server3:
+      # 为了测试server1到server2的NS会不会被server3收到.
+      kind: linux
+      image: 192.168.2.100:5000/nettool
+      exec:
+      - ip a a 55.1.5.11/24 dev net0
+      - ip -6 a a 55:1:5::11/64 dev net0
+      - ip l s dev net0 address aa:c1:ab:00:00:55
+
 
   links:
     - endpoints: ["br-pool0:eth1", "server1:net0"]
     - endpoints: ["br-pool0:eth2", "server2:net0"]
+    - endpoints: ["br-pool0:eth3", "server3:net0"]
 
 EOF
 
