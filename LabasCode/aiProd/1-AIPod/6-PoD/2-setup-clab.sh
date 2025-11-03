49e52caf2ee13e4ee6e3f81a4e13ffbdced334ce
rowan <olaf.luo@foxmail.com>
2025-11-03 19:05:55 +0800

    Add spine-leaf arch for reference

---
+++ LabasCode/aiProd/1-AIPod/6-PoD/2-setup-clab.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -v
+cat <<EOF>clab.yaml | clab deploy -t clab.yaml -
+name: cilium-bgp
+topology:
+  nodes:
+    spine0:
+      kind: linux
+      image: 192.168.2.100:5000/vyos/vyos:1.4.9
+      cmd: /sbin/init
+      binds:
+        - /lib/modules:/lib/modules
+        - ./startup-conf/spine0-boot.cfg:/opt/vyatta/etc/config/config.boot
+
+    spine1:
+      kind: linux
+      image: 192.168.2.100:5000/vyos/vyos:1.4.9
+      cmd: /sbin/init
+      binds:
+        - /lib/modules:/lib/modules
+        - ./startup-conf/spine1-boot.cfg:/opt/vyatta/etc/config/config.boot
+
+    leaf0:
+      kind: linux
+      image: 192.168.2.100:5000/vyos/vyos:1.4.9
+      cmd: /sbin/init
+      binds:
+        - /lib/modules:/lib/modules
+        - ./startup-conf/leaf0-boot.cfg:/opt/vyatta/etc/config/config.boot
+
+    leaf1:
+      kind: linux
+      image: 192.168.2.100:5000/vyos/vyos:1.4.9
+      cmd: /sbin/init
+      binds:
+        - /lib/modules:/lib/modules
+        - ./startup-conf/leaf1-boot.cfg:/opt/vyatta/etc/config/config.boot
+
+
+    server1:
+      kind: linux
+      image: 192.168.2.100:5000/nettool
+      network-mode: container:cilium-bgp-control-plane
+      exec:
+      - ip addr add 10.1.5.10/24 dev net0
+      - ip route replace default via 10.1.5.1
+
+    server2:
+      kind: linux
+      image: 192.168.2.100:5000/nettool
+      network-mode: container:cilium-bgp-worker
+      exec:
+      - ip addr add 10.1.5.11/24 dev net0
+      - ip route replace default via 10.1.5.1
+
+    server3:
+      kind: linux
+      image: 192.168.2.100:5000/nettool
+      network-mode: container:cilium-bgp-worker2
+      exec:
+      - ip addr add 10.1.8.10/24 dev net0
+      - ip route replace default via 10.1.8.1
+
+    server4:
+      kind: linux
+      image: 192.168.2.100:5000/nettool
+      network-mode: container:cilium-bgp-worker3
+      exec:
+      - ip addr add 10.1.8.11/24 dev net0
+      - ip route replace default via 10.1.8.1
+
+  links:
+    # leaf0 --- server1 net0 [vlan 5]
+    - endpoints: ["leaf0:eth5", "server1:net0"]
+      mtu: 1500
+    # leaf0 --- server2 net0 [vlan 5]
+    - endpoints: ["leaf0:eth6", "server2:net0"]
+      mtu: 1500
+    # leaf1 --- server3 net0 [vlan 8]
+    - endpoints: ["leaf1:eth8", "server3:net0"]
+      mtu: 1500
+    # leaf1 --- server4 net0 [vlan 8]
+    - endpoints: ["leaf1:eth9", "server4:net0"]
+      mtu: 1500
+
+    # leaf0 --- spine0
+    - endpoints: ["leaf0:eth1", "spine0:eth1"]
+      mtu: 1500
+    # leaf0 --- spine1
+    - endpoints: ["leaf0:eth2", "spine1:eth1"]
+      mtu: 1500
+    # leaf1 --- spine0
+    - endpoints: ["leaf1:eth1", "spine0:eth2"]
+      mtu: 1500
+    # leaf1 --- spine1
+    - endpoints: ["leaf1:eth2", "spine1:eth2"]
+      mtu: 1500
+EOF
